0b4b382412321c0c2985c9da06602497c989ad7c
johnlucia <johnmlucia@gmail.com>
2018-11-05 13:10:00 -0800

    race it

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ app.post('/rendered_page', function(req, res){
     const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
 
-    await page.goto(pageURL, {
-      waitUntil: 'networkidle2'
-    });
+    await page.goto(pageURL)
 
-    // await setTimeout(async function() { return true }, 2000);
+    await Promise.race([
+      page.waitForNavigation({waitUntil: 'networkidle0'}),
+      page.waitFor(5000)
+    ]);
 
     let theDOM = await page.evaluate(() => document.documentElement.outerHTML);
 
