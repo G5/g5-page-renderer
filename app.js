81236ed6a181cdfcf59535a88f3b66fef00e8420
anonrose <esornona@gmail.com>
2018-12-20 14:55:44 -0800

    update puppeteer api uses

--- app.js
+++ app.js
@@ -1,55 +1,44 @@
-var express = require('express');
+var app = require("express")();
 const PORT = process.env.PORT;
-var debug = require('debug');
 var bodyParser = require("body-parser");
-const puppeteer = require('puppeteer');
-var app = express();
+const puppeteer = require("puppeteer");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-  res.send("G5 Page Renderer");
-});
+app.post("/rendered_page", async function(req, res) {
+  var pageURL = req.body.url;
 
-app.post('/rendered_page', function(req, res){
-  var pageURL=req.body.url;
+  // configure Puppeteer
+  let browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--single-process"
+    ]
+  });
 
-  async function getPage() {
-    // configure Puppeteer
-    let browser = await puppeteer.launch({ headless: true, 
-                                           args: ['--no-sandbox',
-                                                  '--disable-setuid-sandbox',
-                                                  '--disable-dev-shm-usage',
-                                                  '--single-process'] });
-    let page = await browser.newPage();
+  let page = await browser.newPage();
 
-    // Let's speed this thing up by not loading any images
-    await page.setRequestInterception(true);
-    page.on('request', request => {
-      if (request.resourceType() === 'image')
-        request.abort();
-      else
-        request.continue();
-    });
+  await page.setRequestInterception(true);
 
-    // Visit Page URL
-    await page.goto(pageURL)
+  page.on("request", req => {
+    const whitelist = ["document", "script", "xhr", "fetch"];
+    if (!whitelist.includes(req.resourceType())) {
+      return request.abort();
+    }
+    request.continue();
+  });
 
-    // Wait for network to go idle, or up to 5 seconds, whichever comes first
-    await Promise.race([
-      page.waitForNavigation({waitUntil: 'networkidle0'}),
-      page.waitFor(5000)
-    ]);
+  await page.goto(pageURL, { waitUntil: "domcontentLoaded" });
 
-    // Grab the <html> element, close the browser, call it good.
-    let theDOM = await page.evaluate(() => document.documentElement.outerHTML);
-    await browser.close();
-    return theDOM
-  }
+  const theDOM = await page.evaluate(() => document.documentElement.outerHTML);
 
-  getPage().then((body)=>{res.send(body);});
-  
+  res.send(theDOM);
+
+  await browser.close();
 });
 
 app.listen(PORT);
